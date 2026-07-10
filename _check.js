5e552f9315603a43ded7eb720031d025a68d25f0
ste1lgod <z.baze@yandex.ru>
2026-07-10 22:19:54 +0500

    auto: agent file changes (2026-07-10 22:19:53)

--- _check.js
+++ _check.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const html = fs.readFileSync('C:/Users/zbaze/ToiletAdvisor/index.html', 'utf8');
-const matches = [...html.matchAll(/(?s)<script>([\s\S]*?)<\/script>/g)];
+const matches = [...html.matchAll(/<script>([\s\S]*?)<\/script>/g)];
 const combined = matches.map(m => m[1]).join('\n');
 try {
   new Function(combined);
