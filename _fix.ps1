11a632ecd30604aff67d0f3178e8cf1fbdce0f75
ste1lgod <z.baze@yandex.ru>
2026-07-11 01:52:26 +0500

    auto: agent file changes (2026-07-11 01:52:26)

--- _fix.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content "C:\Users\zbaze\ToiletAdvisor\index.html" -Raw -Encoding UTF8
-$content = $content -replace 'tagIco">[^<]*</span><span>Мыло', 'tagIco">🧼</span><span>Мыло'
-Set-Content "C:\Users\zbaze\ToiletAdvisor\index.html" $content -Encoding UTF8 -NoNewline
-Write-Host "Done"
