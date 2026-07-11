019381dbe17c19e336b758865436ea6d3eea89c7
ste1lgod <z.baze@yandex.ru>
2026-07-11 17:11:47 +0500

    auto: agent file changes (2026-07-11 17:11:47)

--- _chk.js
+++
@@ -1,4 +0,0 @@
-const fs=require('fs');
-const html=fs.readFileSync('C:/Users/zbaze/ToiletAdvisor/index.html','utf8');
-const scripts=[...html.matchAll(/<script>([\s\S]*?)<\/script>/g)].map(m=>m[1]).join('\n');
-try{new Function(scripts);console.log('OK');}catch(e){console.error('ERR:',e.message);}
