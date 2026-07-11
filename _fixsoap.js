3f4552c3da70d0d759ea161b4f64b66799b292c6
ste1lgod <z.baze@yandex.ru>
2026-07-11 13:15:12 +0500

    auto: agent file changes (2026-07-11 13:15:12)

--- _fixsoap.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('./index.html', 'utf8');
-// Заменяем повреждённый символ перед </span><span>Мыло
-html = html.replace(/(<span class="tagIco">)[^<]*(<\/span><span>Мыло)/, '$1🧼$2');
-fs.writeFileSync('./index.html', html, 'utf8');
-console.log('Done');
